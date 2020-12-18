b5353167804946ed7e808ec1468aa59ebecf5dfd
Diana <bernabei.d@gmail.com>
2020-12-18 19:36:28 +0100

    [UPDATE] fix function check

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 
 var words = ["HOME", "PANCAKE", "UNNECESSARY", "DOG"];
+var suggestions = ["Where do you live", "Pergect with maple syrup", "!necessary", "The best friend of human"];
 var word = words[Math.floor(Math.random() * words.length)];
+var position = words.indexOf(word);
 console.log(word);
-
 
 for (var i = 0; i < word.length; i++) {
     var node = document.createElement("span");
-    var textnode = document.createTextNode("- ");
+    var textnode = document.createTextNode("-");
     node.appendChild(textnode);
     document.getElementById("randomWord").appendChild(node);
 }
@@ -27,11 +28,13 @@ var flagTrovato = 0;
 function check(onechar){
     flagTrovato = 0;
     onechar = String.fromCharCode(onechar); 
-    console.log(onechar);
     for(var i = 0; i < word.length; i++){
-        if(word[i] == onechar){
-            var elem = document.getElementById("randomWord").getElementsByTagName("span")[i].innerHTML = onechar;
+        var span = document.getElementById("randomWord").getElementsByTagName("span")[i].innerHTML;
+        if(word[i] == onechar && span == "-"){
+            document.getElementById("randomWord").getElementsByTagName("span")[i].innerHTML = onechar
             countOccorrenze ++;
+            flagTrovato = 1;
+        }else if(word[i] == onechar && span != "-"){
             flagTrovato = 1;
         }
     }
@@ -39,15 +42,13 @@ function check(onechar){
         countErrors++;
         document.getElementById("errors").innerHTML = countErrors;
         if(countErrors == 2){
-            console.log("suggerimento");
+            document.getElementById("result").innerHTML = suggestions[position];
         }else if(countErrors == 3){
-            console.log("hai perso");
+            document.getElementById("result").innerHTML = "hai perso!";
         }
     }
-    console.log(countOccorrenze);
-    console.log(countErrors);
 
     if(countOccorrenze == length){
-        console.log("hai vinto");
+        document.getElementById("result").innerHTML = "hai vinto!";
     }
 }
