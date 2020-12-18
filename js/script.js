b50f0fde0da5c0d0cd644e531513ddfe9704a9dc
Diana <bernabei.d@gmail.com>
2020-12-18 20:16:10 +0100

    [ADD] css for the page style and js to draw the man

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 
 var words = ["HOME", "PANCAKE", "UNNECESSARY", "DOG"];
-var suggestions = ["Where do you live", "Pergect with maple syrup", "!necessary", "The best friend of human"];
+var suggestions = ["Where do you live", "Perfect with maple syrup", "!necessary", "The best friend of human"];
 var word = words[Math.floor(Math.random() * words.length)];
 var position = words.indexOf(word);
 console.log(word);
 
+var c = document.getElementById("myCanvas");
+var context = c.getContext("2d");
+context.beginPath();
+context.moveTo(93,303);
+context.lineTo(151,303);
+context.moveTo(123,303);
+context.lineTo(123,110);
+context.moveTo(123,110);
+context.lineTo(247,110);
+context.moveTo(247,110);
+context.lineTo(247,141);
+context.moveTo(247, 162.5 - 23.5);
+context.bezierCurveTo(247 + (0.5522847498307936 * 27), 162.5 - 23.5,  247 + 27, 162.5 - (0.5522847498307936 * 23.5), 247 + 27, 162.5);
+context.bezierCurveTo(247 + 27, 162.5 + (0.5522847498307936 * 23.5), 247 + (0.5522847498307936 * 27), 162.5 + 23.5, 247, 162.5 + 23.5);
+context.bezierCurveTo(247 - (0.5522847498307936 * 27), 162.5 + 23.5, 247 - 27, 162.5 + (0.5522847498307936 * 23.5), 247 - 27, 162.5);
+context.bezierCurveTo(247 - 27, 162.5 - (0.5522847498307936 * 23.5), 247 - (0.5522847498307936 * 27), 162.5 - 23.5, 247, 162.5 - 23.5);
+context.stroke();
+
 for (var i = 0; i < word.length; i++) {
     var node = document.createElement("span");
-    var textnode = document.createTextNode("-");
+    var textnode = document.createTextNode("_");
     node.appendChild(textnode);
     document.getElementById("randomWord").appendChild(node);
 }
@@ -17,6 +35,9 @@ for( var i=65; i<= 90; i++){
     node.setAttribute("onclick", "check("+i+")");
     var textnode = document.createTextNode(String.fromCharCode(i));
     node.appendChild(textnode);
+    // if(i == 75 || i==85){
+    //     document.getElementById("keyboard").appendChild(document.createElement("br"));
+    // }
     document.getElementById("keyboard").appendChild(node);
 }
 
@@ -30,7 +51,7 @@ function check(onechar){
     onechar = String.fromCharCode(onechar); 
     for(var i = 0; i < word.length; i++){
         var span = document.getElementById("randomWord").getElementsByTagName("span")[i].innerHTML;
-        if(word[i] == onechar && span == "-"){
+        if(word[i] == onechar && span == "_"){
             document.getElementById("randomWord").getElementsByTagName("span")[i].innerHTML = onechar
             countOccorrenze ++;
             flagTrovato = 1;
@@ -41,14 +62,36 @@ function check(onechar){
     if(flagTrovato == 0){
         countErrors++;
         document.getElementById("errors").innerHTML = countErrors;
+        //add body on canvas
+        var c = document.getElementById("myCanvas");
+        var context = c.getContext("2d");
+        context.moveTo(247,187);
+        context.lineTo(247,245);
+        context.stroke();
         if(countErrors == 2){
-            document.getElementById("result").innerHTML = suggestions[position];
+            //add arms on canvas
+            var c = document.getElementById("myCanvas");
+            var context = c.getContext("2d");
+            context.moveTo(247,187);
+            context.lineTo(230,208);
+            context.moveTo(247,189);
+            context.lineTo(263,207);
+            context.stroke();
+            document.getElementById("result").innerHTML = "Suggestion: "+suggestions[position];
         }else if(countErrors == 3){
-            document.getElementById("result").innerHTML = "hai perso!";
+            //add legs on canvas
+            var c = document.getElementById("myCanvas");
+            var context = c.getContext("2d");
+            context.moveTo(246,245);
+            context.lineTo(233,268);
+            context.moveTo(248,245);
+            context.lineTo(260,264);
+            context.stroke();
+            document.getElementById("result").innerHTML = "Game over :(";
         }
     }
 
     if(countOccorrenze == length){
-        document.getElementById("result").innerHTML = "hai vinto!";
+        document.getElementById("result").innerHTML = "Win!";
     }
 }
